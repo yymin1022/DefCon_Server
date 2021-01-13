4062b132ea910d05175222510c565fb5f7578cc0
yymin1022 <yymin1022@gmail.com>
2021-01-13 06:32:39 +0000

    Update : Add Corona Feature

--- run.py
+++ run.py
@@ -9,16 +9,20 @@ def render_main():
     return render_template('index.html')
 
 @app.route('/member')
-def render_elements():
+def render_member():
     return render_template('member.html')
 
 @app.route('/privacy')
-def render_generic():
+def render_privacy():
     return render_template('privacy.html')
 
 @app.route('/blog_lr')
 def render_blog_lr():
     return render_template('blog_lr.html')
+
+@app.route('/corona')
+def render_corona():
+    return "Hello, World"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
