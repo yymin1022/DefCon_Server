61787dff25b5f857d8c01f253826f271eb9e79af
yymin1022 <yymin1022@gmail.com>
2021-01-13 06:43:11 +0000

    Update Corona : Add Parsing Basement

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
 from flask import Flask, render_template
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 import socket
 app = Flask(__name__)
 
@@ -22,7 +24,18 @@ def render_blog_lr():
 
 @app.route('/corona')
 def render_corona():
-    return "Hello, World"
+    url303 = "https://coronaboard.kr/"
+
+    html = urlopen(url303).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    div_corona = soup.find("div", class_="col-3 col-sm-3 col-md-2 text-center")
+    p_total = div_corona.find_all("p")[0]
+    p_today = div_corona.find_all("p")[1]
+
+    strResult = p_total + " / " + p_today
+
+    return strResult
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
