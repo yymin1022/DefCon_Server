073d4a53d580e2bba60cac70b52453cfe163d396
yymin1022 <yymin1022@gmail.com>
2021-07-17 17:22:24 +0900

    Update run.py : Improved Variable Name

--- run.py
+++ run.py
@@ -57,43 +57,43 @@ def render_work():
 
 def calcByeongsa(days):
     calcValue = days % 5
-    returnValue = ""
+    strResult = ""
     
     if calcValue == 0:
-        returnValue = "1조 2BRK<br/>2조 MID/SWI<br/>3조 1BRK<br/>4조 MOR<br/>5조 AFT"
+        strResult = "1조 2BRK<br/>2조 MID/SWI<br/>3조 1BRK<br/>4조 MOR<br/>5조 AFT"
     elif calcValue == 1:
-        returnValue = "1조 MOR<br/>2조 AFT<br/>3조 2BRK<br/>4조 MID/SWI<br/>5조 1BRK"
+        strResult = "1조 MOR<br/>2조 AFT<br/>3조 2BRK<br/>4조 MID/SWI<br/>5조 1BRK"
     elif calcValue == 2:
-        returnValue = "1조 MID/SWI<br/>2조 1BRK<br/>3조 MOR<br/>4조 AFT<br/>5조 2BRK"
+        strResult = "1조 MID/SWI<br/>2조 1BRK<br/>3조 MOR<br/>4조 AFT<br/>5조 2BRK"
     elif calcValue == 3:
-        returnValue = "1조 AFT<br/>2조 2BRK<br/>3조 MID/SWI<br/>4조 1BRK<br/>5조 MOR"
+        strResult = "1조 AFT<br/>2조 2BRK<br/>3조 MID/SWI<br/>4조 1BRK<br/>5조 MOR"
     elif calcValue == 4:
-        returnValue = "1조 1BRK<br/>2조 MOR<br/>3조 AFT<br/>4조 2BRK<br/>5조 MID/SWI"
+        strResult = "1조 1BRK<br/>2조 MOR<br/>3조 AFT<br/>4조 2BRK<br/>5조 MID/SWI"
     
-    return returnValue
+    return strResult
 
 def calcGanbu(days):
     calcValue = days % 8
-    returnValue = ""
+    strResult = ""
     
     if calcValue == 0:
-        returnValue = "A조 S/B<br/>B조 2MID/1SWI<br/>C조 1BRK<br/>D조 1DAY"
+        strResult = "A조 S/B<br/>B조 2MID/1SWI<br/>C조 1BRK<br/>D조 1DAY"
     elif calcValue == 1:
-        returnValue = "A조 1MID<br/>B조 2SWI<br/>C조 2BRK<br/>D조 2DAY"
+        strResult = "A조 1MID<br/>B조 2SWI<br/>C조 2BRK<br/>D조 2DAY"
     elif calcValue == 2:
-        returnValue = "A조 2MID/1SWI<br/>B조 1BRK<br/>C조 1DAY<br/>D조 S/B"
+        strResult = "A조 2MID/1SWI<br/>B조 1BRK<br/>C조 1DAY<br/>D조 S/B"
     elif calcValue == 3:
-        returnValue = "A조 2SWI<br/>B조 2BRK<br/>C조 2DAY<br/>D조 1MID"
+        strResult = "A조 2SWI<br/>B조 2BRK<br/>C조 2DAY<br/>D조 1MID"
     elif calcValue == 4:
-        returnValue = "A조 1BRK<br/>B조 1DAY<br/>C조 S/B<br/>D조 2MID/1SWI"
+        strResult = "A조 1BRK<br/>B조 1DAY<br/>C조 S/B<br/>D조 2MID/1SWI"
     elif calcValue == 5:
-        returnValue = "A조 2BRK<br/>B조 2DAY<br/>C조 1MID<br/>D조 2SWI"
+        strResult = "A조 2BRK<br/>B조 2DAY<br/>C조 1MID<br/>D조 2SWI"
     elif calcValue == 6:
-        returnValue = "A조 1DAY<br/>B조 S/B<br/>C조 2MID/1SWI<br/>D조 1BRK"
+        strResult = "A조 1DAY<br/>B조 S/B<br/>C조 2MID/1SWI<br/>D조 1BRK"
     elif calcValue == 7:
-        returnValue = "A조 2DAY<br/>B조 1MID<br/>C조 2SWI<br/>D조 2BRK"
+        strResult = "A조 2DAY<br/>B조 1MID<br/>C조 2SWI<br/>D조 2BRK"
     
-    return returnValue
+    return strResult
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
