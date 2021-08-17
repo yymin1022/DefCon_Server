0315cd198fbdc79f38d5fc6c7d5b0df90317d3c7
yymin1022 <yymin1022@gmail.com>
2021-08-17 20:37:07 +0900

    Update Default.js : Add Header

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -14,6 +14,7 @@ const Default = (props) => {
     
     return (
         <div id="Container" className={className}>
+            <Header/>
             <Section>
                 {children}
             </Section>
