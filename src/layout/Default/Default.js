8a379eb918043991da6f108a8a287ef402394ce8
yymin1022 <yymin1022@gmail.com>
2021-08-17 20:36:46 +0900

    Add Header Component

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Header from "./Header";
 import Section from "./Section";
 
 import "./Default.css"
---
+++ src/layout/Default/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+    
+    return (
+        <div className="Header">
+            <p>Header</p>
+        </div>
+    );
+}
+
+export default Header;
