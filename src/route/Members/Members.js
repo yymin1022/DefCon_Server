7dc8b801ba04ddd144a1bb23b6b6916c46037d67
yymin1022 <yymin1022@gmail.com>
2021-08-17 19:30:49 +0900

    Update Files

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./Project.css";
+import "./Members.css";
 
 const Project = () => {
     return(
--- src/route/Privacy/Privacy.js
+++ src/route/Privacy/Privacy.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./Project.css";
+import "./Privacy.css";
 
 const Project = () => {
     return(
