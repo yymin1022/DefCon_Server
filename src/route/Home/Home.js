d9bb935598dcc9e9e5741e90a925f8d228f6a981
yymin1022 <yymin1022@gmail.com>
2021-08-18 12:55:22 +0900

    Update Home.css : Add Introduction Text element Styling

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -21,7 +21,11 @@ const Home = () => {
                 </div>
 
                 <div id="divIntroductionText">
-                    Introduction Text
+                    <p id="pIntroductionTextTitle">이거 님이 만드신 거였군요!</p>
+                    <p id="pIntroductionTextContent">대학생 프로그래밍팀 DEF:CON은 서울 성보고등학교에서 프로그래밍 동아리로 시작되었습니다.<br/>
+                    우리는 사용자가 우리가 만든 애플리케이션임을 알았을 때<br/>
+                    견고한 신뢰를 가질 수 있는 애플리케이션을 만들고자 힘씁니다.<br/>
+                    2021년, DEF:CON이 새로운 도약을 하려 합니다!</p>
                 </div>
             </div>
             <div className="divHomeContents" id="divAward">
