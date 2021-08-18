02a17b690621551950323c66c1a8b0cb5e3e469c
yymin1022 <yymin1022@gmail.com>
2021-08-18 09:46:00 +0900

    Update Home : Add Basic Elements

--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -7,6 +7,21 @@ const Home = () => {
     return(
         <div>
             <p>Home Page</p>
+            <div className="divHomeContents" id="divIntroduction">
+                Introduction Zone
+            </div>
+            <div className="divHomeContents" id="divAward">
+                Award Zone
+            </div>
+            <div className="divHomeContents" id="divWork">
+                Work Zone
+            </div>
+            <div className="divHomeContents" id="divProject">
+                Project Zone
+            </div>
+            <div className="divHomeContents" id="divContact">
+                Contact Us
+            </div>
             <Link to={"/members"}>Members</Link>
             <Link to={"/privacy"}>Privacy</Link>
         </div>
