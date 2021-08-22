8e1e8fc631fb74ab0d48df5218817b3389bf9f15
Seungmin Jeong <seungmin1266@gmail.com>
2021-08-22 21:05:41 +0900

    Add Members Profile Image

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Members.css";
-import CrabLogo from "../../images/Defcon_crab.svg";
+import Lr from "../../images/LR.png";
+import Haren from "../../images/HarenKei.jpeg";
+import Caret from "../../images/caret.png";
+import V4bel from "../../images/v4bel.jpeg";
 
 function Profile(props) {
     return (
       <div id="divIntro">
-          <div id="divIntroductionImage">
-              <img src={CrabLogo} />
+          <div id="divMemberImage">
+              <img src={ props.img }/>
           </div>
           <div id="divMemberIntroduction">
           <h2>{ props.name }</h2>
           <p><span>{ props.school }</span></p>
           <p>{ props.intro }</p>
           </div>
+          <br></br>
       </div>
         
     );
@@ -22,10 +26,10 @@ const Members = () => {
         <div id="divMemberIntroduction">
             <p><span id="spanMembersBar">Members</span></p>
             <div id="divMember">
-                <Profile name = {"LR"} school = {"중앙대학교"} intro = {"누구보다 보라매를 사랑하는 남자"}/>
-                <Profile name = {"HarenKei"} school = {"대림대학교"} intro = {"땅개"}/>
-                <Profile name = {"Caret"} school = {"안양대학교"} intro = {"킹익"}/>
-                <Profile name = {"V4BEL"} school = {"NULL"} intro = {"확대범"}/>
+                <Profile img = {Lr} name = {"LR"} school = {"중앙대학교 19학번"} intro = {"대한민국 공군 ROKAF"}/>
+                <Profile img = {Haren} name = {"HarenKei"} school = {"대림대학교 19학번"} intro = {"대한민국 육군 ROKA"}/>
+                <Profile img = {Caret} name = {"Caret"} school = {"안양대학교 19학번"} intro = {"대한민국 사회복무요원"}/>
+                <Profile img = {V4bel} name = {"V4BEL"} school = {"NULL"} intro = {"Defnit팀 소속으로 CTF 참가"}/>
             </div>
         </div>
     )
