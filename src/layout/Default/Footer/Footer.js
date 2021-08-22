416806e9b827e70131ef8e0b7cdb6b74832d8c3e
yymin1022 <yymin1022@gmail.com>
2021-08-22 10:45:48 +0900

    Add Footer component

---
+++ src/layout/Default/Footer/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import DefconLogo from "../../../images/Defcon_logo_default.svg";
+
+import "./Footer.css"
+
+const Footer = () => {
+    
+    return (
+        <div className="Footer">
+            <div id="footerLogo">
+                <img src={DefconLogo} />
+            </div>
+
+            <div id="footerNav">
+                <Link to={"/"}>
+                    <p>Home</p>
+                </Link>
+                <Link to={"/members"}>
+                    <p>Members</p>
+                </Link>
+                <Link to={"/privacy"}>
+                    <p>Privacy</p>
+                </Link>
+                <Link to={"/projects"}>
+                    <p>Project</p>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
