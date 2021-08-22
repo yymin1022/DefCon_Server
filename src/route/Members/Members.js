dd36415c482fa0cd76a9385e140f37213a886a91
Seungmin Jeong <seungmin1266@gmail.com>
2021-08-22 11:30:17 +0900

    Add img of DEFCON Crab

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Members.css";
+import CrabLogo from "../../images/Defcon_crab.svg";
 
 function profile(name, school,intro) {
     return (
       <div>
+          <div id="divIntroductionImage">
+              <img src={CrabLogo} />
+          </div>
+          <div id="divMemberIntroduction">
           <h2>name</h2>
           <p><span>school</span></p>
           <p>intro</p>
+          </div>
       </div>
         
     );
@@ -16,7 +22,6 @@ const Members = () => {
         <>
             <div id="divMember">
                 { profile('yymin1022', '중앙대학교', '황제')}
-                
             </div>
         </>
     )
