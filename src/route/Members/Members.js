1446780a8f2d30d3b07ac04165a8db31cf7e33cf
Seungmin Jeong <seungmin1266@gmail.com>
2021-08-22 11:20:48 +0900

    Add Components at members.js

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -1,12 +1,22 @@
 import React from "react";
-
 import "./Members.css";
 
+function profile(name, school,intro) {
+    return (
+      <div>
+          <h2>name</h2>
+          <p><span>school</span></p>
+          <p>intro</p>
+      </div>
+        
+    );
+  }
 const Members = () => {
     return(
         <>
             <div id="divMember">
-                Member Zone
+                { profile('yymin1022', '중앙대학교', '황제')}
+                
             </div>
         </>
     )
