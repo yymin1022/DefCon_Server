a323c594b84938b90b01d63aed702f6ca3d83465
yymin1022 <yymin1022@gmail.com>
2021-08-23 15:28:54 +0900

    Update Footer.js : Change Logo to Split Logo

--- src/layout/Default/Footer/Footer.js
+++ src/layout/Default/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import DefconLogoSplit from "../../images/Defcon_logo_split.svg";
+import DefconLogoSplit from "../../../images/Defcon_logo_split.svg";
 
 import "./Footer.css"
 
