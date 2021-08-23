2d3aef18f0f764ab769a7e4601b57c1449b2ccca
Seungmin Jeong <seungmin1266@gmail.com>
2021-08-23 18:05:56 +0900

    Edited some CSS code

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -5,6 +5,7 @@ import Haren from "../../images/HarenKei.jpeg";
 import Caret from "../../images/caret.png";
 import V4bel from "../../images/v4bel.jpeg";
 
+
 function Profile(props) {
     return (
       <div id="divIntro">
@@ -12,24 +13,24 @@ function Profile(props) {
               <img src={ props.img }/>
           </div>
           <div id="divMemberIntroduction">
-          <h2>{ props.name }</h2>
+          <h2>{ props.nickname }  |  <span id="spanName">{ props.name }</span></h2>
           <p><span>{ props.group }</span></p>
           <p>{ props.intro }</p>
           </div>
           <br></br>
       </div>
-        
     );
   }
+
 const Members = () => {
     return(
         <div id="divMemberIntroduction">
             <p><span id="spanMembersBar">Members</span></p>
             <div id="divMember">
-                <Profile img = {Lr} name = {"LR"} group = {"중앙대학교 19학번 / 대한민국 공군 ROKAF "} intro = {"집에가고싶겠다"}/>
-                <Profile img = {Haren} name = {"HarenKei"} group = {"대림대학교 19학번 / 대한민국 육군 ROKA "} intro = {"코딩하십쇼 저누님..."}/>
-                <Profile img = {Caret} name = {"Caret"} group = {"안양대학교 19학번 / 대한민국 사회복무요원 ROK Social Agent "} intro = {"Caret이 [ ] 하면 망한다"}/>
-                <Profile img = {V4bel} name = {"V4BEL"} group = {"Defnit"} intro = {"킹왕짱 해커"}/>
+                <Profile img = {Lr} nickname = {"LR"} name = {"유용민"} group = {"중앙대학교 19학번 / 대한민국 공군 ROKAF "} intro = {"Android, Web"}/>
+                <Profile img = {Haren} nickname = {"HarenKei"} name = {"정승민"} group = {"대림대학교 19학번 / 대한민국 육군 ROKA "} intro = {"Python Chatbot, Design, Web, iOS"}/>
+                <Profile img = {Caret} nickname = {"Caret"} name = {"김영현"} group = {"안양대학교 19학번 / 대한민국 사회복무요원 ROK Social Agent "} intro = {"Android, Web"}/>
+                <Profile img = {V4bel} nickname = {"V4BEL"} name = {"김현우"} group = {"Defnit"} intro = {"킹왕짱 해커"}/>
             </div>
         </div>
     )
