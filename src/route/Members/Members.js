09de3d9cf8ba92f1e335b3556ebf088568f4f4b1
yymin1022 <yymin1022@gmail.com>
2021-08-25 18:13:12 +0900

    Update Members.js : Cleaned UP

--- src/route/Members/Members.js
+++ src/route/Members/Members.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import "./Members.css";
-import Lr from "../../images/LR.png";
-import Haren from "../../images/HarenKei.jpeg";
-import Caret from "../../images/caret.png";
-import V4bel from "../../images/v4bel.jpeg";
-import Gesaegi from "../../images/Defcon_crab.svg";
-import LrRecord from "../../record/yymin1022.json";
-import HrRecord from "../../record/haren.json";
-import CrRecord from "../../record/caret.json";
-import VbRecord from "../../record/v4bel.json";
 
+import imgLR from "../../images/LR.png";
+import imgHaren from "../../images/HarenKei.jpeg";
+import imgCaret from "../../images/caret.png";
+import imgV4bel from "../../images/v4bel.jpeg";
+import CrabLogo from "../../images/Defcon_crab.svg";
+import ProfileData from "./ProfileData.json";
+
+import "./Members.css";
 
 function Profile(props) { {/*프로필 컴포넌트 함수*/} 
     return (
@@ -28,46 +26,74 @@ function Profile(props) { {/*프로필 컴포넌트 함수*/}
   }
 
   
-const Members = () => {
-    const [MemberRecord, setMemberRecord] = useState(0);
-    const viewLrRecord = () => {
+const Members=() => {
+    const [MemberRecord, setMemberRecord]=useState(0);
+    const viewLrRecord=() => {
         console.log(LrRecord);
     }
-    const viewHrRecord = () => {
+    const viewHrRecord=() => {
         console.log(HrRecord);
     }
-    const viewCrRecord = () => {
+    const viewCrRecord=() => {
         console.log(CrRecord);
     }
-    const viewVbRecord = () => {
+    const viewVbRecord=() => {
         console.log(VbRecord);
     }
 
     return(
         <div id="divMemberIntroduction">
-            <div id="divMemberTitle">{/*멤버 페이지 제목 바를 위한 div*/}
-                    <p id="pMemberTitle"> <span id="spanMemberTitle">Members</span></p>
-            </div>
-            <div id="divContainer"> {/*이 div는 자식요소들을 묶기 위한 flex-container로 작용함*/}
-            <div id="divMember"> {/*멤버의 소개를 보여주는 div*/}
-                <div id="yym" onClick={viewLrRecord}>
-                <Profile img = {Lr} nickname = {"LR"} name = {"유용민"} group = {"중앙대학교 19학번 / 대한민국 공군 ROKAF "} intro = {"Android, Web"}/>
-                </div>
-                <div id ="jsm" onClick={viewHrRecord}>
-                <Profile img = {Haren} nickname = {"HarenKei"} name = {"정승민"} group = {"대림대학교 19학번 / 대한민국 육군 ROKA "} intro = {"Python Chatbot, Design, Web, iOS"}/>
-                </div>
-                <div id ="kyh" onClick={viewCrRecord}>
-                <Profile img = {Caret} nickname = {"Caret"} name = {"김영현"} group = {"안양대학교 19학번 / 대한민국 사회복무요원 ROK Social Agent "} intro = {"Android, Web"}/>
-                </div>
-                <div id="khw" onClick={viewVbRecord}>
-                <Profile img = {V4bel} nickname = {"V4BEL"} name = {"김현우"} group = {"Defnit"} intro = {"킹왕짱 해커"}/>
-                </div>
+            {/*멤버 페이지 제목 바를 위한 div*/}
+            <div id="divMemberTitle">
+                <p id="pMemberTitle">
+                    <span id="spanMemberTitle">
+                        Members
+                    </span>
+                </p>
             </div>
 
-            <div id="divCrab"> {/*게새기와 멤버들의 이력을 배치하기 위한 div*/}
-                <img src={ Gesaegi } />
-                
-            </div>
+            {/*이 div는 자식요소들을 묶기 위한 flex-container로 작용함*/}
+            <div id="divContainer">
+                {/*멤버의 소개를 보여주는 div*/}
+                <div id="divMember">
+                    <div id="yym" onClick={viewLrRecord}>
+                        <Profile
+                            img={Lr}
+                            nickname={"LR"}
+                            name={"유용민"}
+                            group={"중앙대학교 19학번 / 대한민국 공군 ROKAF "}
+                            intro={"Android, Web"} />
+                    </div>
+                    <div id ="jsm" onClick={viewHrRecord}>
+                        <Profile
+                            img={Haren}
+                            nickname={"HarenKei"}
+                            name={"정승민"}
+                            group={"대림대학교 19학번 / 대한민국 육군 ROKA "}
+                            intro={"Python Chatbot, Design, Web, iOS"} />
+                    </div>
+                    <div id ="kyh" onClick={viewCrRecord}>
+                        <Profile
+                            img={Caret}
+                            nickname={"Caret"}
+                            name={"김영현"}
+                            group={"안양대학교 19학번 / 대한민국 사회복무요원 ROK Social Agent "}
+                            intro={"Android, Web"}/>
+                    </div>
+                    <div id="khw" onClick={viewVbRecord}>
+                        <Profile
+                            img={V4bel}
+                            nickname={"V4BEL"}
+                            name={"김현우"}
+                            group={"Defnit"}
+                            intro={"킹왕짱 해커"}/>
+                    </div>
+                </div>
+
+                {/*게새기와 멤버들의 이력을 배치하기 위한 div*/}
+                <div id="divCrab">
+                    <img src={CrabLogo} />
+                </div>
             </div>
         </div>
     )
