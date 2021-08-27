ba9396934aa910268d91a974f62a231e266d6a52
yymin1022 <yymin1022@gmail.com>
2021-08-27 20:27:27 +0900

    Update Projects.js : Apply JSON Structure

--- src/route/Projects/Projects.js
+++ src/route/Projects/Projects.js
@@ -1,26 +1,27 @@
 import React from "react";
 
+import ProjectData from "./ProjectData.json";
+
 import "./Projects.css";
 
 const Projects = () => {
     return(
         <>
             <dl>
-                <dt><h2>&gt;_Android Application</h2></dt>
-                <dd><p>&gt;_택시미터기 (2019) - LR</p></dd>
-                <dd><p>&gt;_SoulRest (2019)</p></dd>
-                <dd><p>&gt;_More...</p></dd>
-                <br/><br/>
-                <dt><h2>&gt;_Web</h2></dt>
-                <dd><p>&gt;_DEF:CON Website (2021)</p></dd>
-                <dd><p>&gt;_LR's IT BLOG (2021)</p></dd>
-                <dd><p>&gt;_More...</p></dd>
-                <br/><br/>
-                <dt><h2>&gt;_Self Repair</h2></dt>
-                <dd><p>&gt;_iPhone 6s Assemble (2019) - LR, HarenKei</p></dd>
-                <dd><p>&gt;_iPhone X Assemble (2020) - LR</p></dd>
-                <dd><p>&gt;_G-SHOCK 배터리 교환 (2020) - LR, HarenKei</p></dd>
-                <dd><p>&gt;_More...</p></dd>
+                {
+                    ProjectData.map(item => {
+                        <>
+                            <dt><h2>&gt;_{item.type}</h2></dt>
+
+                            {item.projects.map(projectItem => {
+                                <dd><p>&gt;_{projectItem.title} ({projectItem.year}) - {projectItem.user}</p></dd>
+                            })}
+
+                            <dd><p>&gt;_More...</p></dd>
+                            <br/><br/>
+                        </>
+                    })
+                }
             </dl>
         </>
     )
